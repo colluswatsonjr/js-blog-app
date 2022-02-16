1f64277e25de58e276b1adcadc402fac375a554d
Collus Watson <cklwatsonjr@gmail.com>
2022-02-15 20:35:15 -0500

    add selector event listener, function to create card

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,27 +1,61 @@
 // alert('hello world!')
-// grabs form, inputs, selectors by ID
+// grabs form, inputs, selectors, containers, buttons by ID
 const createCardForm = () => document.getElementById('createCardForm');
 const titleField = () => document.getElementById('cardTitleField');
-const authorField = ()=> document.getElementById('cardAuthorField');
-const contentField = ()=> document.getElementById('cardContentField');
+const authorField = () => document.getElementById('cardAuthorField');
+const contentField = () => document.getElementById('cardContentField');
 const showCards = () => document.getElementById('showCards');
 const holdCards = () => document.getElementById('holdCards');
+const editCard = () => document.getElementById('editCard')
+const deleteCard = () => document.getElementById('deleteCard');
+
+let cardList = [];
+//Functions
+function createCard(data) {
+    console.log('got data')
+
+    let newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerHTML = `
+    <h2 id="cardTitle">${data.title}</h2>
+    <h4 id="cardAuthor">${data.author}</h4>
+    <h4 id="cardContent">${data.content}</h4>
+    <button type="submit" id="editCard">Edit Card</button>
+    <button type="submit" id="deleteCard">Delete Card</button>
+    `;
+
+    displayCard(newCard)
+}
+
+function displayCard(card){
+    
+}
+
 
 //Eventlisteners
 createCardForm().addEventListener('submit', (event) => {
     event.preventDefault();
+    console.log('heard submit')
+
     let cardData = {
         title: titleField().value,
         author: authorField().value,
-        content: authorField().value
+        content: contentField().value
     }
 
-    console.log('heard submit', cardData)
+    cardList.unshift(cardData) //add to created card list
+    createCard(cardData) //create div card to add to html
+
 })
 
-showCards().addEventListener('change', (event)=>{
+showCards().addEventListener('change', (event) => {
     event.preventDefault();
-    let selected = event.target.value
+    console.log('heard change')
 
-    console.log('heard change', selected)
+    let selected = event.target.value;
+    displayCard();
 })
+
+//Renders
+
+// displayCards();
