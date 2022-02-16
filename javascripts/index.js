421efe1e00aba5574fbf58eba0d5d94a40372225
Collus Watson <cklwatsonjr@gmail.com>
2022-02-15 20:01:30 -0500

    add const to grab forms, input fields, selectors, card container add Eventlisteners to form submit, select change

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,27 @@
 // alert('hello world!')
+// grabs form, inputs, selectors by ID
+const createCardForm = () => document.getElementById('createCardForm');
+const titleField = () => document.getElementById('cardTitleField');
+const authorField = ()=> document.getElementById('cardAuthorField');
+const contentField = ()=> document.getElementById('cardContentField');
+const showCards = () => document.getElementById('showCards');
+const holdCards = () => document.getElementById('holdCards');
+
+//Eventlisteners
+createCardForm().addEventListener('submit', (event) => {
+    event.preventDefault();
+    let cardData = {
+        title: titleField().value,
+        author: authorField().value,
+        content: authorField().value
+    }
+
+    console.log('heard submit', cardData)
+})
+
+showCards().addEventListener('change', (event)=>{
+    event.preventDefault();
+    let selected = event.target.value
+
+    console.log('heard change', selected)
+})
