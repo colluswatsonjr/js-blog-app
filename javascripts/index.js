87d60046a686bf6a9c4eb44023c5806c6535255a
Collus Watson <cklwatsonjr@gmail.com>
2022-02-15 21:19:03 -0500

    add temporaray data to arrays, create function to display cards created, updata select to change set of cards dispalyed when changed

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,11 +9,32 @@ const holdCards = () => document.getElementById('holdCards');
 const editCard = () => document.getElementById('editCard')
 const deleteCard = () => document.getElementById('deleteCard');
 
-let cardList = [];
+let cardList = [
+    {
+        title: 'title1',
+        author: 'author1',
+        content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
+    },
+    {
+        title: 'title2',
+        author: 'author2',
+        content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
+    }
+];
+let notCardList =[
+    {
+        title: 'title3',
+        author: 'author3',
+        content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
+    },
+    {
+        title: 'title4',
+        author: 'author4',
+        content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
+    }];
 //Functions
-function createCard(data) {
+function createCard(data) { //function to create card from data
     console.log('got data')
-
     let newCard = document.createElement('div')
     newCard.classList.add('card')
     newCard.innerHTML = `
@@ -23,17 +44,20 @@ function createCard(data) {
     <button type="submit" id="editCard">Edit Card</button>
     <button type="submit" id="deleteCard">Delete Card</button>
     `;
-
-    displayCard(newCard)
+    return newCard;
 }
 
-function displayCard(card){
-    
+function displayCards(cardSet){ //function to display cards from array
+    holdCards().innerHTML = '';
+    cardSet.forEach(card => {
+        let created = createCard(card);
+        holdCards().appendChild(created)
+    })
 }
 
 
 //Eventlisteners
-createCardForm().addEventListener('submit', (event) => {
+createCardForm().addEventListener('submit', (event) => { //add submit listenr to form
     event.preventDefault();
     console.log('heard submit')
 
@@ -45,17 +69,24 @@ createCardForm().addEventListener('submit', (event) => {
 
     cardList.unshift(cardData) //add to created card list
     createCard(cardData) //create div card to add to html
-
 })
 
-showCards().addEventListener('change', (event) => {
+showCards().addEventListener('change', (event) => { //add change listener to select
     event.preventDefault();
     console.log('heard change')
-
     let selected = event.target.value;
-    displayCard();
+    if(selected == 'currentCards'){
+        // console.log(cardList)
+        displayCards(cardList)
+    }else if(selected == 'deletedCards'){
+        // console.log(notCardList)
+        displayCards(notCardList)
+    }else{
+        console.log('neither selected')
+    }
+    
 })
 
 //Renders
 
-// displayCards();
+displayCards(cardList);
