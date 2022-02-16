9411ddc10f580262ff0f32e0f05fa51395bb6372
Collus Watson <cklwatsonjr@gmail.com>
2022-02-16 18:54:52 -0500

    add click listeners to edit, remove, restore buttons.

--- javascripts/index.js
+++ javascripts/index.js
@@ -16,23 +16,39 @@ function createCard(data) { //function to create card from data
     console.log('got data')
     let newCard = document.createElement('div')
     newCard.classList.add('card');
-    newCard.id = data.id
+    newCard.id = '';
     newCard.innerHTML = `
     <h2 id="cardTitle">${data.title}</h2>
     <h4 id="cardAuthor">${data.author}</h4>
     <h4 id="cardContent">${data.content}</h4>
     <button id="editCard">Edit Card</button>
-    <button id="deleteCard">Delete Card</button>
+    <button id="restoreCard">Retore Card</button>
+    <button id="removeCard">Remove Card</button>
     `;
     newCard.querySelector('#editCard').addEventListener('click', () => {
-        console.log(newCard)
+        
     })
-    newCard.querySelector('#deleteCard').addEventListener('click', () => {
-        console.log(newCard)
+    newCard.querySelector('#restoreCard').addEventListener('click', () => {
+        deleteData(data, 'removedCards');
+        data.id = '';
+        postData(data, 'currentCards');
+        fetchData();
+    })
+    newCard.querySelector('#removeCard').addEventListener('click', () => {
+        if (confirm("Are you sure?")) {
+            if (showCards().value == 'currentCards') {
+                console.log('remove')
+                deleteData(data, 'currentCards');
+                data.id = '';
+                postData(data, 'removedCards');
+            } else if (showCards().value == 'removedCards') {
+                deleteData(data)
+            }
+            fetchData();
+        }
     })
     return newCard;
 }
-
 function displayCards() { //function to display cards from array
     fetchData();
     // holdCards().innerHTML = '';
@@ -53,40 +69,57 @@ function displayCards() { //function to display cards from array
     // }
 
 }
-
 //Fetch
 // http://localhost:3000/currentCards
-// http://localhost:3000/deletedCards
-function fetchData(select = showCards().value) { //fetch data from database
+// http://localhost:3000/removedCards
+function fetchData(selectList = showCards().value) { //fetch data from database
     holdCards().innerHTML = '';
-    fetch(`http://localhost:3000/${select}`)
+    fetch(`http://localhost:3000/${selectList}`)
         .then(res => res.json())
         .then(data => {
             data.forEach(card => {
                 let created = createCard(card);
+                if (selectList == 'currentCards') {
+                    created.querySelector('#restoreCard').style.display = 'none';
+                    created.querySelector('#removeCard').innerHTML = 'Remove Card'
+
+                } else if (selectList == 'removedCards') {
+                    created.querySelector('#removeCard').innerHTML = 'Delete Card'
+                }
                 holdCards().appendChild(created)
             })
         })
         .catch(err => console.log(err))
 };
 
-function postData(cardData){ //add data to database using fetch
-    fetch(`http://localhost:3000/currentCards`,{
-        method:'POST',
-        headers:{
-            'Accept':'application/json',
-            'Content-Type':'application/json'
+function postData(cardData, selectList = showCards().value) { //add data to database using fetch
+    fetch(`http://localhost:3000/${selectList}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(cardData)
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
+function deleteData(cardData, selectList = showCards().value) {
+    console.log(cardData)
+    fetch(`http://localhost:3000/${selectList}/${cardData.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.log(err))
 }
 
 //Eventlisteners
 createCardForm().addEventListener('submit', (event) => { //add submit listenr to form
-    debugger;
     event.preventDefault();
 
     let cardData = {
@@ -94,8 +127,8 @@ createCardForm().addEventListener('submit', (event) => { //add submit listenr to
         author: authorField().value,
         content: contentField().value
     }
-    
-    postData(cardData); //add data to database
+
+    postData(cardData, 'currentCards'); //add data to database
     fetchData(); //display all cards of currently selected 
 })
 
