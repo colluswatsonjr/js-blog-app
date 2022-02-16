38720bf9337370a49340d37b25d84f9235a1e624
Collus Watson <cklwatsonjr@gmail.com>
2022-02-15 23:12:30 -0500

    add DOMcontentloaded to render cards. add click event listener to editBtn and deleteBtn

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,16 +7,18 @@ const contentField = () => document.getElementById('cardContentField');
 const showCards = () => document.getElementById('showCards');
 let selected = showCards().value
 const holdCards = () => document.getElementById('holdCards');
-const editCard = () => document.getElementById('editCard')
-const deleteCard = () => document.getElementById('deleteCard');
+// const editCard = () => document.querySelector('#editCard');
+// const deleteCard = () => document.querySelector('#deleteCard');
 
 let cardList = [
     {
+        id: 1,
         title: 'title1',
         author: 'author1',
         content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
     },
     {
+        id: 2,
         title: 'title2',
         author: 'author2',
         content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
@@ -24,31 +26,41 @@ let cardList = [
 ];
 let notCardList = [
     {
+        id: 3,
         title: 'title3',
         author: 'author3',
         content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
     },
     {
+        id: 4,
         title: 'title4',
         author: 'author4',
         content: 'contentekdfnjnjvnvn wjfvwjefv wjvwfvwfvvwev'
     }];
+
 //Functions
 function createCard(data) { //function to create card from data
     console.log('got data')
     let newCard = document.createElement('div')
-    newCard.classList.add('card')
+    newCard.classList.add('card');
+    newCard.id = data.id
     newCard.innerHTML = `
     <h2 id="cardTitle">${data.title}</h2>
     <h4 id="cardAuthor">${data.author}</h4>
     <h4 id="cardContent">${data.content}</h4>
-    <button type="submit" id="editCard">Edit Card</button>
-    <button type="submit" id="deleteCard">Delete Card</button>
+    <button id="editCard">Edit Card</button>
+    <button id="deleteCard">Delete Card</button>
     `;
+    newCard.querySelector('#editCard').addEventListener('click', ()=>{
+        console.log(newCard)
+    })
+    newCard.querySelector('#deleteCard').addEventListener('click', ()=>{
+        console.log(newCard)
+    })
     return newCard;
 }
 
-function displayCards(selected) { //function to display cards from array
+function displayCards(selected = 'currentCards') { //function to display cards from array
     holdCards().innerHTML = '';
     if (selected == 'currentCards') {
         cardList.forEach(card => {
@@ -90,6 +102,9 @@ showCards().addEventListener('change', (event) => { //add change listener to sel
     return selected;
 })
 
-//Renders
 
-displayCards(selected);
+//Renders
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    displayCards();
+})
